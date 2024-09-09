fbcc5f5a8415b829b5d1acc88f88ba72fc0261e1
ThunPao <52374829+ThunPao@users.noreply.github.com>
2024-09-09 19:44:06 +0700

    add how to docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <>
       <div className="container mx-auto   py-16 space-y-4 px-4 lg:px-40 bg-base-100">
 
-        <div className="text-5xl font-bold">Todo List : รายการที่ต้องทำ</div>
+        <div className="text-4xl font-bold">Todo List : รายการที่ต้องทำ</div>
           <Suspense fallback={<TaskLoadSkeletonPage/>}>
             <TaskList fetchData={() => fetchTasks()} />
           </Suspense>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -18,7 +18,7 @@ export default async function SearchPage({ searchParams }: SearchProps) {
     return (
         <>
             <div className="container mx-auto   py-16 space-y-4 px-4 lg:px-40">
-                <div className="text-5xl font-bold">Todo List</div>
+                <div className="text-4xl font-bold">Todo List : รายการที่ต้องทำ</div>
                 <Suspense fallback={<TaskLoadSkeletonPage />}>
                     <TaskList fetchData={() => fetchSearchTasks(term)} />
                 </Suspense>
